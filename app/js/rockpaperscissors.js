79bbb585d41feb5771a83ba5e24457a74098f96b
Kyle Cooper <kylcooper22@gmail.com>
2015-09-20 14:03:32 -0400

    Added unit 4 homework

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,10 +1,10 @@
 ////////////////////////////////////////////////
 /*   Provided Code - Please Don't Edit   */
 ////////////////////////////////////////////////
-'use strict';
+
 
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
+    console.log("Please choose either 'rock', 'paper', or 'scissors'.");
     return prompt();
 }
 function randomPlay() {
@@ -40,7 +40,25 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if (playerMove == 'rock' && computerMove == 'rock'){
+        winner = 'tie';
+    }   else if (playerMove == 'rock' && computerMove == 'paper'){
+        winner = 'computer';
+    }   else if (playerMove == 'rock' && computerMove == 'scissors') {
+        winner = 'player';
+    }   else if (playerMove == 'scissors' && computerMove == 'scissors'){
+        winner = 'tie';
+    }   else if (playerMove == 'scissors' && computerMove == 'rock'){
+        winner = 'computer';
+    }   else if (playerMove == 'scissors' && computerMove == 'paper') {
+        winner = 'player';
+    }   else if (playerMove == 'paper' && computerMove == 'paper'){
+        winner = 'tie';
+    }   else if (playerMove == 'paper' && computerMove == 'scissors'){
+        winner = 'computer';
+    }   else if (playerMove == 'paper' && computerMove == 'rock') {
+        winner = 'player';
+    }   else winner = 'error';
     return winner;
 }
 
